9682d21ca5b767dc6d61f10f4aca40d502067789
core.editor <andreeavillatte@gmail.com>
2019-04-15 09:24:24 +0200

    ajout verification si la saisie était fait et que les valeurs sont des nombres

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-function multiplication(){
+function multiplication() {
     // Récuperation des variables
-var firstNumber=document.getElementById('firstNumber').value;
-var secondNumber=document.getElementById('secondNumber').value;
-// avec la fonction MathTrunc j'ai tronqué seulement le premier nombre
-var firstNumber=Math.trunc(firstNumber);
-// j'ai multiplié le premier nombre tronqué avec le deuxième nombre que je n'ai pas touché
-multiplication = firstNumber * secondNumber;
-// je mets la condition de faire le calcul seulement si la saisie était faite corecte: si une valeure était introduites et si elle est un chiffre
-if (!firstNumber || !secondNumber || isNaN(firstNumber) || isNaN(secondNumber)){
-    alert('Hey!!!!');
-}else{
-    alert('1er nombre : ' + firstNumber + '\n2ème nombre : ' + secondNumber + '\nMultiplication : ' + multiplication);
-}
+    var firstNumber = document.getElementById('firstNumber').value;
+    var secondNumber = document.getElementById('secondNumber').value;
+    // avec la fonction MathTrunc j'ai tronqué seulement le premier nombre
+    var firstNumber = Math.trunc(firstNumber);
+    // j'ai multiplié le premier nombre tronqué avec le deuxième nombre que je n'ai pas touché
+    multiplication = firstNumber * secondNumber;
+    // je mets la condition de faire le calcul seulement si la saisie était faite corecte: si une valeure était introduites et si elle est un chiffre
+    if (!firstNumber || !secondNumber || isNaN(firstNumber) || isNaN(secondNumber)) {
+        alert('Hey!!!!');
+    } else {
+        alert('1er nombre : ' + firstNumber + '\n2ème nombre : ' + secondNumber + '\nMultiplication : ' + multiplication);
+    }
 }
 
 
