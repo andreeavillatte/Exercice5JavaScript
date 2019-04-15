c3acfa70cb0c685e07adc08565188a149647ec3e
core.editor <andreeavillatte@gmail.com>
2019-04-15 09:21:02 +0200

    ajout verification si la saisie était fait et que les valeurs sont des nombres

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var firstNumber=Math.trunc(firstNumber);
 // j'ai multiplié le premier nombre tronqué avec le deuxième nombre que je n'ai pas touché
 multiplication = firstNumber * secondNumber;
 // je mets la condition de faire le calcul seulement si la saisie était faite corecte: si une valeure était introduites et si elle est un chiffre
-if (isNaN(firstNumber) || isNaN(secondNumber)){
+if (!firstNumber || !secondNumber || isNaN(firstNumber) || isNaN(secondNumber)){
     alert('Hey!!!!');
 }else{
     alert('1er nombre : ' + firstNumber + '\n2ème nombre : ' + secondNumber + '\nMultiplication : ' + multiplication);
